9aac8a3676f0b9e66c55d423be681e2b3b4d635d
PRD <32349731+fakeGenuis@users.noreply.github.com>
2024-07-22 20:05:21 +0800

    add --remove option to mmaVS.sh

--- mmaVS.sh
+++ mmaVS.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://support.wolfram.com/12473?src=mathematica
+# TODO seems there is another binary call `ELProver` in new versions of `mathematica`
 BINARYS=(math mcc mathematica wolfram Mathematica MathKernel WolframKernel)
 MMA_PATH=/usr/local/Wolfram/Mathematica/
 
@@ -11,17 +13,44 @@ link_bin() {
     ln -sfv $MMA_PATH"$ver"/SystemFiles/Kernel/Binaries/Linux-x86-64/wolframscript /usr/bin/wolframscript
 }
 
-CUR_VERSION=$(realpath "$(which Mathematica)" | cut -d'/' -f6)
-echo "Current Mathematica version ${CUR_VERSION}."
-
-echo "Select another version:"
+# do this if you want a `mathematica` installation with aur helper
+rm_links() {
+    for bi in "${BINARYS[@]}"; do
+        ls -l "$(which "${bi:?}")" && rm -fv "$(which "${bi:?}")" || exit
+    done
+    ls -l "$(which wolframscript)" && rm -fv "$(which wolframscript)" || exit
+}
 
 other_vers() {
     cur=$1
-    find $MMA_PATH -maxdepth 1 -mindepth 1 -type d ! -name "*$cur" -printf "%f\n"
+    find $MMA_PATH -maxdepth 1 -mindepth 1 -type d ! -name "$cur" -printf "%f\n"
 }
-select ver in $(other_vers "$CUR_VERSION"); do
-    echo "Switch to Mathematica version ${ver}."
-    link_bin "$ver"
-    break
-done
+
+switch_vers() {
+    CUR_VERSION=$(realpath "$(which Mathematica)" | cut -d'/' -f6)
+    [[ -n "$CUR_VERSION" ]] && echo "Current Mathematica version ${CUR_VERSION}." || echo "Not available Mathematica linked!"
+
+    echo "Select another version:"
+    select ver in $(other_vers "$CUR_VERSION"); do
+        echo "Switch to Mathematica version ${ver}."
+        link_bin "$ver"
+        break
+    done
+}
+
+case "$1" in
+    --remove)
+        rm_links
+        ;;
+    --switch)
+        switch_vers
+        ;;
+    *)
+        cat <<EOF
+sudo ./mmaVS.sh [Options]
+change version or remove links of mathematica in your \$PATH.
+
+--remove     remove links
+--switch     change version (by relink)
+EOF
+esac
